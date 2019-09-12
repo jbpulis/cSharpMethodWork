dfb6e10da29a3eb182431afde19ba371b2e7fbe5
Jason <jbpulis@yahoo.com>
2019-09-12 14:24:28 -0500

    entry method work
    very basic stuff to start off

--- MethodWork/MethodWork/Program.cs
+++ MethodWork/MethodWork/Program.cs
@@ -6,7 +6,22 @@ namespace MethodWork
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteSomething();
+            WriteSomethingSpecific("I am and argument and am called from a method!" + "\n");
         }
+        //access modifier, (static), return type, method name (parameter1, parameter2,...)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method.");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
+        //----------------------------------------------------
+
+
     }
 }
