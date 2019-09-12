fc789c0bbd67c8645b62ebb9ad620f20e707df8d
Jason <jbpulis@yahoo.com>
2019-09-12 14:48:10 -0500

    working with return values
    basic math methods

--- MethodWork/MethodWork/Program.cs
+++ MethodWork/MethodWork/Program.cs
@@ -8,6 +8,11 @@ namespace MethodWork
         {
             WriteSomething();
             WriteSomethingSpecific("I am and argument and am called from a method!" + "\n");
+
+            Console.WriteLine(Add(15,31));
+            Console.WriteLine(Add(Add(1, 2), Add(10, 5)));
+            Console.WriteLine(Multiply(2, 3));
+            Console.WriteLine(Divide(25, 13));
         }
         //access modifier, (static), return type, method name (parameter1, parameter2,...)
         public static void WriteSomething()
@@ -18,6 +23,23 @@ namespace MethodWork
         public static void WriteSomethingSpecific(string myText)
         {
             Console.WriteLine(myText);
+        }
+
+        //----------------------------------------------------
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
         }
 
         //----------------------------------------------------
