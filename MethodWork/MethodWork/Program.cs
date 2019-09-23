6cf684caa7ed012abe268883506766915243115d
Jason <jbpulis@yahoo.com>
2019-09-23 14:50:58 -0500

    Try/Catch/Exceptions
    using try and catch, specifically to catch exceptions, including input exceptions and divide by zero

--- MethodWork/MethodWork/Program.cs
+++ MethodWork/MethodWork/Program.cs
@@ -9,19 +9,52 @@ namespace MethodWork
             WriteSomething();
             WriteSomethingSpecific("I am and argument and am called from a method!" + "\n");
 
+
             Console.WriteLine(Add(15,31));
             Console.WriteLine(Add(Add(1, 2), Add(10, 5)));
             Console.WriteLine(Multiply(2, 3));
             Console.WriteLine(Divide(25, 13));
             Console.WriteLine("\n");
 
+
             HelloFriend("Jeff", "Amanda", "Tyson");
             Console.WriteLine("\n");
+
 
             Console.WriteLine("Enter your name: ");
             string input = Console.ReadLine();
             Console.WriteLine(input);
             Addition();
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("Please enter a number: ");
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exceeption, please enter the correct type next time!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, the number was too long or too short for an int32.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNull exception, the value was empty(null).");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyways!");
+            }
+            Division();
+
+
+
+
         }
         //access modifier, (static), return type, method name (parameter1, parameter2,...)
         public static void WriteSomething()
@@ -76,6 +109,24 @@ namespace MethodWork
 
         //------------------------------------------------------
 
+        public static void Division()
+        { 
 
+            Console.WriteLine("Enter the value of the dividend:");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of the divisor:");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int quotient = input1 / input2;
+                Console.WriteLine("The quotient of " + input1 + " / " + input2 + " is " + quotient);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("DivideByZero Exception, division by zero is not possible.");
+            }
+        }
+
+        //-------------------------------------------------------
     }
 }
